3d848b6353dc0917b0e08824278b39e68cf214a1
Neil Babson <nbabson@pdx.edu>
2018-08-05 01:13:36 -0700

    added player

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+/* Neil Babson
+*  August 2018
+*/
+
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 mod util;
 use util::*; 
 use std::collections::HashMap;
+use std::io::{self, Write, BufRead};
 
 fn make_game_objects(objects: &mut HashMap<String, Item>) {
-     let hat: Item = Item::new("old fedora".to_string(), 2.0);
+     let hat: Item = Item::new("hat".to_string(), "old fedora".to_string(), 2.0);
      objects.insert("hat".to_string(), hat);
-     objects.insert("crystal ball".to_string(), Item::new("grapefruit sized sparkling crystaline orb".to_string(), 6.5));
-     
-     println!("{:?}", objects);
-}
-
-
-fn make_world(world: &mut Vec<Location>, objects: &mut HashMap<String, Item>) {
-
+     objects.insert("crystal ball".to_string(), 
+             Item::new("crystal ball".to_string(), "grapefruit sized sparkling crystalline orb".to_string(), 6.5));
+     //println!("{:?}", objects);
 }
 
  #[test]
@@ -26,12 +29,56 @@ fn make_world(world: &mut Vec<Location>, objects: &mut HashMap<String, Item>) {
      assert_eq!(6.5, w.weight);
  }
 
+fn make_world(world: &mut Vec<Location>, objects: &mut HashMap<String, Item>) {
+    let mut items: Vec<Item> = Vec::new();
+    items.push(objects.remove("hat").unwrap()); 
+    let mut exits: Vec<Exit> = Vec::new();
+    exits.push(Exit::new("A low archway to the south".to_string(), 1, Direction::S));
+    exits.push(Exit::new("A ladder in the northeast corner".to_string(), 2, Direction::NE)); 
+    world.push(Location::new(0, "Leafy Courtyard".to_string(), 
+                 "This pleasant courtyard is enclosed by vine covered brick walls and contains ".to_string() +
+                 "a gaily splashing fountain at its center.", items, false, exits)); 
+    //println!("{:?}", world);
+}
+
+#[test]
+fn object_is_in_location() {
+    let mut w: Vec<Location> = Vec::new();
+    let mut o: HashMap<String, Item> = HashMap::new();
+    make_game_objects(&mut o);
+    make_world(&mut w, &mut o);
+    let hat: Item = Item::new("hat".to_string(), "old fedora".to_string(), 2.0);
+    assert_eq!(true, w[0].items.contains(&hat));
+}
+
+fn make_player(objects: &mut HashMap<String, Item>) -> Player {
+    print!("Welcome! What is your name? ");
+    io::stdout().flush().ok().unwrap();
+    let stdin = io::stdin();
+    let name = stdin.lock().lines().next().unwrap().unwrap();
+    let mut starting_inventory: Vec<Item> = Vec::new();
+    starting_inventory.push(objects.remove("crystal ball").unwrap());
+    Player::new(name, starting_inventory)
+}
+
+#[test]
+fn player_has_object() {
+    let mut o: HashMap<String, Item> = HashMap::new();
+    make_game_objects(&mut o);
+    let p = make_player(&mut o);
+    println!("Test name");
+    io::stdout().flush().ok().unwrap();
+    let ball: Item = Item::new("crystal ball".to_string(), "grapefruit sized sparkling crystalline orb".to_string(), 6.5);
+    assert_eq!(true, p.inventory.contains(&ball));
+}
 
 fn main() {
     let mut world: Vec<Location> = Vec::new(); 
     let mut objects: HashMap<String, Item> = HashMap::new();
     make_game_objects(&mut objects);
     make_world(&mut world, &mut objects);
+    let player = make_player(&mut objects);
+
     let l: Location;
-    test();
+    //test();
 }
--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,10 @@
+/* Neil Babson
+*  August 2018
+*/
 
+#[derive(Debug)]
+pub enum Direction {
+    N, S, E, W, NE, SE, NW, SW, }
 
 #[derive(Debug)]
 pub struct Location {
@@ -10,15 +16,27 @@ pub struct Location {
     pub exits: Vec<Exit>,
 }    
 
-#[derive(Debug)]
+impl Location {
+    pub fn new(ind: usize, n: String, d: String, it: Vec<Item>, v: bool, e: Vec<Exit>) -> Location {
+        Location { index: ind,
+                   name: n,
+                   description: d,
+                   items: it,
+                   visited: v,
+                   exits: e, }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Item {
+    pub name: String,
     pub description: String,
     pub weight: f32,
 }    
 
 impl Item {
-    pub fn new(d: String, w: f32) -> Item {
-        Item { description: d, weight: w}
+    pub fn new(n: String, d: String, w: f32) -> Item {
+        Item { name: n, description: d, weight: w}
     }
 }
 
@@ -26,10 +44,28 @@ impl Item {
 pub struct Exit {
     description: String,
     goes_to: usize,    
+    direction: Direction,
     //goes_to: &'a Location,
 }                 
 
-
-pub fn test() {
-    println!("test");
+impl Exit {
+    pub fn new(d: String, g: usize, dir: Direction) -> Exit {
+        Exit { description: d, goes_to: g, direction: dir}
+    }
 }
+
+#[derive(Debug)]
+pub struct Player {
+    pub name: String,
+    pub inventory: Vec<Item>,
+    pub score: i32,
+}
+
+
+impl Player {
+    pub fn new(n: String, i: Vec<Item>) -> Player {
+        Player { name: n, inventory: i, score: 0 }
+    }
+}    
+
+
